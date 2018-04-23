24751cc339ac58d9fec7543e25b2e05b1000d4d2
Jake Woratzeck <31188226+JakeWoratzeck@users.noreply.github.com>
2018-04-23 15:03:40 -0500

    Added logout.php file

---
+++ HTML Files/logout.php
@@ -0,0 +1,21 @@
+<?php
+	if(!session_start()) {
+		header("Location: error.php");
+		exit;
+	}
+	
+	$_SESSION = array();
+	
+	if (ini_get("session.use_cookies")) {
+		$params = session_get_cookie_params();
+		setcookie(session_name(), '', 1,
+			$params["path"], $params["domain"],
+			$params["secure"], $params["httponly"]
+		);
+	}
+	
+	session_destroy();
+	
+	header("Location: login.php");
+	exit;
+?>
