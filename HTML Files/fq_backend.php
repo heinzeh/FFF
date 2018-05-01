6276c08d29180274e96c5265ed8eae5bc7ff1bd8
Jake Woratzeck <31188226+JakeWoratzeck@users.noreply.github.com>
2018-05-01 15:05:38 -0500

    Added fq_backend.php

---
+++ HTML Files/fq_backend.php
@@ -0,0 +1,67 @@
+<?php
+	
+	if(!session_start()) {
+		header("Location: error.php");
+		exit;
+	}
+	
+	$loggedIn = empty($_SESSION['loggedin']) ? false : $_SESSION['loggedin'];
+	
+	if ($loggedIn) {
+		header("Location: fq_backend.php");
+		exit;
+	}
+	
+	
+	/*$action = empty($_POST['action']) ? '' : $_POST['action'];
+	//If user didn't get to this page from registration_form.php, send them there	
+	if ($action == 'do_registration') {
+		handle_registration();
+	} else {
+		registration_form();
+	}*/
+
+	function handle_registration() {
+		
+		$console = empty($_POST['console']) ? '' : $_POST['console'];
+		$gameType = empty($_POST['gameType']) ? '' : $_POST['gameType'];
+		
+		$con = mysqli_connect('localhost' ,'root','cs4320','CS4320_Final_Project');
+		
+		if (!$con) {
+		    die('Could not connect: ' . mysqli_error($con));
+		}
+		
+		//define $query_sql
+				
+		if($console != "" && $gameType != "") {
+			
+			if(!mysqli_query($con,$query_sql)){
+				$error = "Could not make list";
+				require "friendQuery.php";
+				return;
+			}
+			else {
+				//$_SESSION['loggedin'] = $username;
+				header("Location: fq_backend.php");
+			}
+			
+		}
+		else {
+			$error = "Please fill out all fields.";
+			require "friendQuery.php";
+			return;
+		}
+	}
+				
+
+
+	
+	/*function registration_form() {
+		$username = "";
+		$error = "";
+		require "friendQuery.php";
+		return;
+	}*/
+	
+?>
