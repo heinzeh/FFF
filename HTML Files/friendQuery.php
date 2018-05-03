c3a2d3256caabc5ee1e4b64cc477aeca8aea1d0f
Jake Woratzeck <31188226+JakeWoratzeck@users.noreply.github.com>
2018-05-03 15:27:24 -0500

    Made first option on dropdown unselectable

--- HTML Files/friendQuery.php
+++ HTML Files/friendQuery.php
@@ -37,7 +37,7 @@ function showUser(str) {
 
 <form>
 <select name="users" onchange="showUser(this.value)">
-  <option value="">Select your Gametype:</option>
+  <option disabled selected value="">Select your Gametype:</option>
   <option value=1>Duos</option>
   <option value=2>Squads</option>
   </select>
