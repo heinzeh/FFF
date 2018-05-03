09a5df3126663b8922f1302adc536e059fb11bae
Jake Woratzeck <31188226+JakeWoratzeck@users.noreply.github.com>
2018-05-03 14:27:32 -0500

    Added testing variables

--- HTML Files/friendQuery.php
+++ HTML Files/friendQuery.php
@@ -25,8 +25,8 @@ if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 
-mysqli_select_db($con,"ajax_demo");
-$sql="SELECT * FROM user WHERE id = '".$q."'";
+mysqli_select_db($con,"CS4320_Final_Project");
+$sql="SELECT * FROM members WHERE id = '".$q."'";
 $result = mysqli_query($con,$sql);
 
 echo "<table>
@@ -39,11 +39,11 @@ echo "<table>
 </tr>";
 while($row = mysqli_fetch_array($result)) {
     echo "<tr>";
-    echo "<td>" . $row['FirstName'] . "</td>";
-    echo "<td>" . $row['LastName'] . "</td>";
-    echo "<td>" . $row['Age'] . "</td>";
-    echo "<td>" . $row['Hometown'] . "</td>";
-    echo "<td>" . $row['Job'] . "</td>";
+    echo "<td>" . $row['firstName'] . "</td>";
+    echo "<td>" . $row['lastName'] . "</td>";
+    echo "<td>" . $row['username'] . "</td>";
+    echo "<td>" . $row['console'] . "</td>";
+    echo "<td>" . $row['gamertag'] . "</td>";
     echo "</tr>";
 }
 echo "</table>";
